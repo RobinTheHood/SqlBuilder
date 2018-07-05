24f44754b977d98cc1eed1ebadf0339c3045848b
Robin Wieschendorf <mail@robinwieschendorf.de>
2018-07-05 09:27:33 +0200

    Added lessThanOrEqual and greaterThanOrEqual

--- src/SqlWhere.php
+++ src/SqlWhere.php
@@ -4,6 +4,8 @@ namespace RobinTheHood\SqlBuilder;
 class SqlWhere extends SqlBuilder
 {
     const OP_EQUAL = '=';
+    const OP_LESS_THAN_OR_EQUAL = '<=';
+    const OP_GREATER_THAN_OR_EQUAL = '>=';
 
     private $values;
 
@@ -13,12 +15,27 @@ class SqlWhere extends SqlBuilder
         $this->root = $root;
     }
 
+    public function lessThanOrEqual($column, $value)
+    {
+        return $this->setOperation($column, $value, self::OP_LESS_THAN_OR_EQUAL);
+    }
+
+    public function greaterThanOrEqual($column, $value)
+    {
+        return $this->setOperation($column, $value, self::OP_GREATER_THAN_OR_EQUAL);
+    }
+
     public function equals($column, $value)
+    {
+        return $this->setOperation($column, $value, self::OP_EQUAL);
+    }
+
+    public function setOperation($column, $value, $operator)
     {
         $this->values[] = [
                 'column' => $column,
                 'value' => $value,
-                'operator' => self::OP_EQUAL
+                'operator' => $operator
             ];
         return $this;
     }
@@ -42,6 +59,10 @@ class SqlWhere extends SqlBuilder
         foreach($this->values as $value) {
             if ($value['operator'] == self::OP_EQUAL) {
                 $expression .=  "(`{$value['column']}` = {$this->root->nextMapping($value['value'])}) ";
+            } elseif ($value['operator'] == self::OP_LESS_THAN_OR_EQUAL) {
+                $expression .=  "(`{$value['column']}` <= {$this->root->nextMapping($value['value'])}) ";
+            } elseif ($value['operator'] == self::OP_GREATER_THAN_OR_EQUAL) {
+                $expression .=  "(`{$value['column']}` >= {$this->root->nextMapping($value['value'])}) ";
             }
 
             if (++$count < count($this->values)) {
