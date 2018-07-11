770b23cfa0a77c82a046e9d976b6ec9a70cea79c
Robin Wieschendorf <mail@robinwieschendorf.de>
2018-07-11 16:52:01 +0200

    Addes subWhere()

--- src/SqlWhere.php
+++ src/SqlWhere.php
@@ -6,13 +6,21 @@ class SqlWhere extends SqlBuilder
     const OP_EQUAL = '=';
     const OP_LESS_THAN_OR_EQUAL = '<=';
     const OP_GREATER_THAN_OR_EQUAL = '>=';
+    const OP_AND = 'AND';
+    const OP_OR = 'OR';
 
     private $values;
+    private $isSubWhere;
+    private $subWheres;
+    private $boolOperator;
 
-    public function __construct($root)
+    public function __construct($root, $isSubWhere = false, $operator = self::OP_AND)
     {
         $this->values = [];
         $this->root = $root;
+        $this->isSubWhere = $isSubWhere;
+        $this->subWheres = [];
+        $this->boolOperator = $operator;
     }
 
     public function lessThanOrEqual($column, $value)
@@ -28,6 +36,19 @@ class SqlWhere extends SqlBuilder
     public function equals($column, $value)
     {
         return $this->setOperation($column, $value, self::OP_EQUAL);
+    }
+
+    public function subWhere($boolOperator = 'AND')
+    {
+        $operator = self::OP_AND;
+        if ($boolOperator == 'OR') {
+            $operator = self::OP_OR;
+        }
+
+        $subWhere = new SqlWhere($this->root, true, $operator);
+        $this->subWheres[] = $subWhere;
+
+        return $subWhere;
     }
 
     public function setOperation($column, $value, $operator)
@@ -54,8 +75,39 @@ class SqlWhere extends SqlBuilder
 
     private function sqlExpression()
     {
+        $expressionSubWheres = $this->sqlExpressionSubWheres();
+        $expressionValues = $this->sqlExpressionValues();
+
+        if ($expressionSubWheres && $expressionValues) {
+            $expression = $expressionSubWheres . ' ' . $this->boolOperator . ' ' . $expressionValues;
+        } else {
+            $expression = $expressionSubWheres . $expressionValues;
+        }
+
+        return $expression;
+    }
+
+    private function sqlExpressionSubWheres()
+    {
         $count = 0;
         $expression = '';
+
+        foreach($this->subWheres as $subWhere) {
+            $expression .= '(' . $subWhere->sqlExpression() . ')';
+
+            if (++$count < count($this->subWheres)) {
+                $expression .=  ' ' . $this->boolOperator . ' ';
+            }
+        }
+
+        return $expression;
+    }
+
+    private function sqlExpressionValues()
+    {
+        $count = 0;
+        $expression = '';
+
         foreach($this->values as $value) {
             if ($value['operator'] == self::OP_EQUAL) {
                 $expression .=  "(`{$value['column']}` = {$this->root->nextMapping($value['value'])}) ";
@@ -66,9 +118,10 @@ class SqlWhere extends SqlBuilder
             }
 
             if (++$count < count($this->values)) {
-                $expression .=  "AND ";
+                $expression .=  $this->boolOperator . ' ';
             }
         }
+
         return $expression;
     }
 }
